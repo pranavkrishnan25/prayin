ed69be125d084a7e91b2e533397a0c12c142c5cd
Pranav Krishnan <pkrishnan@unc.edu>
2023-06-28 16:19:27 -0400

    Initial Commit

---
+++ notbs/notbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  notbsApp.swift
+//  notbs
+//
+//  Created by Pranav Krishnan on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct notbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
